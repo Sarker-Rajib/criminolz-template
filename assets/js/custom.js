3203c29ac47bb3cfce5c68439219715deb14b43b
Rajib Sarker <redhat0593@gmail.com>
2023-11-24 20:58:13 +0600

    responsive done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,21 @@
 document.getElementById("info-toggler").addEventListener('click', () => {
     document.getElementById("info").classList.toggle('hide')
+    document.getElementById("info-s").classList.toggle('d-none')
+    document.getElementById("info-h").classList.toggle('d-none')
+})
+
+document.getElementById("left-menu-toggler").addEventListener('click', () => {
+    document.getElementById("leftMenu").classList.toggle('hide')
+})
+
+document.getElementById("CloseBtnLeft").addEventListener('click', () => {
+    document.getElementById("leftMenu").classList.add('hide')
+})
+
+document.getElementById("right-menu-toggler").addEventListener('click', () => {
+    document.getElementById("rigtMenu").classList.toggle('hide')
+})
+
+document.getElementById("CloseBtnRight").addEventListener('click', () => {
+    document.getElementById("rigtMenu").classList.add('hide')
 })
