7e256401f3273df8c85b9f9e10cc20ed32e900b7
Rajib Sarker <redhat0593@gmail.com>
2023-11-25 01:13:52 +0600

    rev sone

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,10 +8,22 @@ document.getElementById("left-menu-toggler").addEventListener('click', () => {
     document.getElementById("leftMenu").classList.toggle('hide')
     document.getElementById("opn1").classList.toggle('d-none')
     document.getElementById("cls1").classList.toggle('d-none')
+    const condition = document.getElementById("opn2").classList.contains('d-none');
+    if (condition == true) {
+        document.getElementById("opn2").classList.remove('d-none')
+        document.getElementById("cls2").classList.add('d-none')
+        document.getElementById("rigtMenu").classList.add('hide')
+    }
 })
 
 document.getElementById("right-menu-toggler").addEventListener('click', () => {
     document.getElementById("rigtMenu").classList.toggle('hide')
     document.getElementById("opn2").classList.toggle('d-none')
     document.getElementById("cls2").classList.toggle('d-none')
+    const condition = document.getElementById("opn1").classList.contains('d-none');
+    if (condition == true) {
+        document.getElementById("opn1").classList.remove('d-none')
+        document.getElementById("cls1").classList.add('d-none')
+        document.getElementById("leftMenu").classList.add('hide')
+    }
 })
