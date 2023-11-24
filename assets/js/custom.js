905e56184d74d715dbce3e925888dbca83c65add
Rajib Sarker <redhat0593@gmail.com>
2023-11-24 23:38:54 +0600

    added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,16 +6,12 @@ document.getElementById("info-toggler").addEventListener('click', () => {
 
 document.getElementById("left-menu-toggler").addEventListener('click', () => {
     document.getElementById("leftMenu").classList.toggle('hide')
-})
-
-document.getElementById("CloseBtnLeft").addEventListener('click', () => {
-    document.getElementById("leftMenu").classList.add('hide')
+    document.getElementById("opn1").classList.toggle('d-none')
+    document.getElementById("cls1").classList.toggle('d-none')
 })
 
 document.getElementById("right-menu-toggler").addEventListener('click', () => {
     document.getElementById("rigtMenu").classList.toggle('hide')
-})
-
-document.getElementById("CloseBtnRight").addEventListener('click', () => {
-    document.getElementById("rigtMenu").classList.add('hide')
+    document.getElementById("opn2").classList.toggle('d-none')
+    document.getElementById("cls2").classList.toggle('d-none')
 })
